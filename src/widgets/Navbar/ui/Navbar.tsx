88ee84c3035472272c95a2e2c1a9134c0cb4254e
Ilya Dolgov <prhatecore@gmail.com>
2023-06-30 14:41:23 +0300

    Create Navbar and custom component AppLink

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,26 @@
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { classNames } from "shared/lib/classNames/classNames";
+import styles from "./Navbar.module.scss";
+
+interface INavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: INavbarProps) => {
+  return (
+    <div className={classNames(styles.Navbar, {}, [className])}>
+      <div className={styles.links}>
+        <AppLink
+          to={"/"}
+          theme={AppLinkTheme.SECONDARY}
+          className={styles.mainLink}
+        >
+          Главная
+        </AppLink>
+        <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>
+          О Сайте
+        </AppLink>
+      </div>
+    </div>
+  );
+};
