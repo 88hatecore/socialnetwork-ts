dcac832480993f4bac518dd1fa5e5ca341d55979
Ilya Dolgov <prhatecore@gmail.com>
2023-07-02 17:07:49 +0300

    Implementing the Svg loader and creating a custom Button component

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,24 @@
+import { Theme, useTheme } from "app/providers/ThemeProvider";
+import { Button, ThemeButton } from "shared/ui/Button/Button";
+import { classNames } from "shared/lib/classNames/classNames";
+import styles from "./ThemeSwitcher.module.scss";
+import LightIcon from "shared/assets/icons/theme-light.svg";
+import DarkIcon from "shared/assets/icons/theme-dark.svg";
+
+interface IThemeSwitcherProps {
+  className?: string;
+}
+
+export const ThemeSwitcher = ({ className }: IThemeSwitcherProps) => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button
+      theme={ThemeButton.CLEAR}
+      className={classNames(styles.ThemeSwitcher, {}, [className])}
+      onClick={toggleTheme}
+    >
+      {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+    </Button>
+  );
+};
