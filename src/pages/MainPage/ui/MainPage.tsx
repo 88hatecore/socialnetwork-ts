36a976f00601562001a99b53fc102296d8558f98
Ilya Dolgov <prhatecore@gmail.com>
2023-07-03 12:32:38 +0300

    Webpack hot module

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,12 +3,7 @@ import { useTranslation } from "react-i18next";
 const MainPage = () => {
   const { t } = useTranslation("about");
 
-  return (
-    <div>
-      {t("Новый перевод")}
-      {t("Главная страница")}
-    </div>
-  );
+  return <div>{t("Главная страница")}</div>;
 };
 
 export default MainPage;
