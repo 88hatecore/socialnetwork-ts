f01052534e76fdd3de57f7bc783c1f513f75b168
Ilya Dolgov <prhatecore@gmail.com>
2023-07-13 01:22:08 +0300

    fix ts lint

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,9 +4,8 @@ import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 import { LangSwitcher } from "widgets/LangSwitcher";
 import { useTranslation } from "react-i18next";
 import { Button } from "shared/ui/Button/Button";
-import styles from "./Sidebar.module.scss";
 import { classNames } from "shared/lib/classNames/classNames";
-
+import styles from "./Sidebar.module.scss";
 
 interface ISidebarProps {
   className?: string;
